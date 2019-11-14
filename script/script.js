262a69c132ab41ea939481e612b78ea6d21d03da
ksawerykarczewski <ksaw0011@stud.kea.dk>
2019-11-14 11:58:25 +0100

    timeline initial working

--- script/script.js
+++ script/script.js
@@ -6,4 +6,33 @@ fetch("timeline.svg")
 
 function loadSVG(data) {
   document.querySelector(".SVG_timeline").innerHTML = data;
+  main();
+}
+
+function main() {
+  const elms = document.querySelectorAll("section");
+
+  const config = {
+    root: null, //document.querySelector('#some-element')
+    rootMargin: "0px",
+    threshold: [0, 0.5, 0.75, 1]
+  };
+
+  let observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      let lastChar = entry.target.id[entry.target.id.length - 1];
+      if (entry.intersectionRatio > 0.35) {
+        entry.target.classList.add("focus");
+        document.querySelector("#circle" + lastChar).style.fill = "pink";
+      } else {
+        entry.target.classList.remove("focus");
+        document.querySelector("#circle" + lastChar).style.fill = "var(--main-bg-color)";
+      }
+      console.log(entry.intersectionRatio);
+    });
+  }, config);
+
+  elms.forEach(elem => {
+    observer.observe(elem);
+  });
 }
