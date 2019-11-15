0ef2e1f63e11284df7aefec803723bd4743bf096
ksawerykarczewski <ksaw0011@stud.kea.dk>
2019-11-15 09:53:11 +0100

    friday initial

--- script/script.js
+++ script/script.js
@@ -11,6 +11,7 @@ function loadSVG(data) {
 
 function main() {
   const elms = document.querySelectorAll("section");
+  const date = document.querySelector("#date_vertical");
 
   const config = {
     root: null, //document.querySelector('#some-element')
@@ -21,18 +22,32 @@ function main() {
   let observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       let lastChar = entry.target.id[entry.target.id.length - 1];
+      let currentDate = entry.target.querySelector(".date").textContent;
+      let currentYear = currentDate.substring(currentDate.length - 4);
       if (entry.intersectionRatio > 0.35) {
-        entry.target.classList.add("focus");
-        document.querySelector("#circle" + lastChar).style.fill = "pink";
+        // entry.target.classList.add("focus");
+        document.querySelector("#circle" + lastChar).style.fill = "red";
+        date.innerHTML = currentYear;
       } else {
-        entry.target.classList.remove("focus");
+        // entry.target.classList.remove("focus");
         document.querySelector("#circle" + lastChar).style.fill = "var(--main-bg-color)";
       }
-      console.log(entry.intersectionRatio);
     });
   }, config);
 
   elms.forEach(elem => {
     observer.observe(elem);
   });
+  let circles = document.querySelectorAll("circle");
+  circles.forEach(circle => {
+    circle.addEventListener("click", scrollToSection);
+  });
+
+  function scrollToSection(e) {
+    let lastCharId = e.target.id[e.target.id.length - 1];
+    let currentSection = document.querySelector("#section" + lastCharId);
+    currentSection.scrollIntoView({
+      block: "start"
+    });
+  }
 }
