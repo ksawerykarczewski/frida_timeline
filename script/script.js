67f2ffdee4f23bca2dd617a86dcc386b2f47725f
ksawerykarczewski <ksaw0011@stud.kea.dk>
2019-11-16 19:32:33 +0100

    modal fixed

--- script/script.js
+++ script/script.js
@@ -63,6 +63,9 @@ const link = "https://spreadsheets.google.com/feeds/list/1RFOqG5y5zjhd7gwNB5fC9W
 const template = document.querySelector("template").content;
 let counter = 1;
 
+const modal = document.querySelector(".modal-bg");
+const modalUl = document.querySelector(".modal-content ul");
+
 fetch("../timeline.svg")
   .then(e => e.text())
   .then(data => loadSVG(data));
@@ -80,6 +83,10 @@ function fetchLink(link) {
 
 function displayEvent(oneEvent) {
   const clone = template.cloneNode("true");
+
+  if (counter == 10) {
+    counter = 0;
+  }
   clone.querySelector("section").id = "section" + counter;
   clone.querySelector("h1").textContent = oneEvent.gsx$heading.$t;
   clone.querySelector(".date").textContent = oneEvent.gsx$date.$t;
@@ -88,7 +95,27 @@ function displayEvent(oneEvent) {
   clone.querySelector(".painting").src = "../imgs/" + oneEvent.gsx$painting.$t;
   clone.querySelector(".quote").textContent = oneEvent.gsx$quote.$t;
   // clone.querySelector("video source").src = "video/" + oneEvent.gsx$video.$t;
+  const newLi = document.createElement("li");
+  const newDiv = document.createElement("div");
+  const newP = document.createElement("p");
+
+  newP.textContent = oneEvent.gsx$heading.$t;
+  newP.id = "category" + counter;
+
+  fetch("imgs/svg/" + oneEvent.gsx$doodlesvg.$t)
+    .then(e => e.text())
+    .then(data => loadSVG(data));
+
+  function loadSVG(data) {
+    newDiv.innerHTML = data;
+  }
+
+  newLi.appendChild(newDiv);
+  newLi.appendChild(newP);
+
+  modalUl.appendChild(newLi);
   counter++;
+
   document.querySelector("main").appendChild(clone);
   main();
 }
@@ -108,12 +135,14 @@ function main() {
       let lastChar = entry.target.id[entry.target.id.length - 1];
       let currentDate = entry.target.querySelector(".date").textContent;
       let currentYear = currentDate.substring(currentDate.length - 4);
-      if (entry.intersectionRatio > 0.35) {
-        entry.target.classList.add("focus");
+      if (entry.intersectionRatio > 0) {
+        document.querySelector("#category" + lastChar).style.textDecoration = "underline";
+        // entry.target.classList.add("focus");
         document.querySelector("#circle" + lastChar).style.fill = "black";
         date.innerHTML = currentYear;
       } else {
-        entry.target.classList.remove("focus");
+        document.querySelector("#category" + lastChar).style.textDecoration = "none";
+        // entry.target.classList.remove("focus");
         document.querySelector("#circle" + lastChar).style.fill = "var(--main-bg-color)";
       }
       // console.log(entry.intersectionRatio);
@@ -130,6 +159,9 @@ function main() {
   });
 
   function scrollToSection(e) {
+    if (!modal.classList.contains("hide")) {
+      modal.classList.add("hide");
+    }
     let lastCharId = e.target.id[e.target.id.length - 1];
     let currentSection = document.querySelector("#section" + lastCharId);
     currentSection.scrollIntoView({
@@ -137,9 +169,17 @@ function main() {
     });
   }
 
-  document.querySelector(".close-btn").addEventListener("click", hideModal);
+  let categories = document.querySelectorAll("li");
+  categories.forEach(cat => {
+    cat.addEventListener("click", scrollToSection);
+  });
 
-  function hideModal() {
-    document.querySelector(".modal-bg").classList.add("hide");
-  }
+  document.querySelector(".close-btn").addEventListener("click", hideModal);
+  document.querySelector(".topnav img").addEventListener("click", showModal);
+}
+function hideModal() {
+  modal.classList.add("hide");
+}
+function showModal() {
+  modal.classList.remove("hide");
 }
